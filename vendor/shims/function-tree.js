d7de6aab1051a57a9aff72f0cd7af794986b0332
lifeart <lifeart92@gmail.com>
2017-10-27 00:30:24 +0300

    shim update

--- vendor/shims/function-tree.js
+++ vendor/shims/function-tree.js
@@ -8,3 +8,19 @@
   // export an object with a default property that contains the 'hljs' global.
   define('function-tree/lib/devtools/base', ['function-tree/devtools/base'], vendorModule);
 })();
+
+
+(function() {
+  /* globals define, self */
+  function vendorModule(base) {
+    'use strict';
+    
+    return {
+      'default': self['base'] || base,
+      debounce: base,
+      __esModule: true,
+    };
+  }
+  // export an object with a default property that contains the 'hljs' global.
+  define('function-tree/factories', ['function-tree/factories/debounce'], vendorModule);
+})();
